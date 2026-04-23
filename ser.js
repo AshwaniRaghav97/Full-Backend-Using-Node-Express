2fca02e89f80386020a8b659ea17bf3f7153edd9
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-04-23 14:01:59 +0530

    Node js server

--- ser.js
+++ ser.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1>Hello Ashwani Raghav</h1>`);
+    res.end(`<h1>Hello Ashwani</h1>`);
 });
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
