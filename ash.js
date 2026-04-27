1a665e4eab7ebdbe002b3387b3dc72a7aa1c2ae3
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-04-27 16:40:54 +0530

    commited

--- ash.js
+++ ash.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1>Hell</h1>`);
+    res.end(`<h1>Hell Ashwani</h1>`);
 });
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
